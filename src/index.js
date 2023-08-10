08be0e1a506e7dcc3d75cb69dca44611b79e02af
Deybear <drodriguezr@fwdcostarica.com>
2023-08-10 15:46:27 -0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,79 @@
 
 // - - - - - - - - - - || IMPORT OBJECTS || - - - - - - - - - - //
+
 import { Keyboard } from "./keyboard.js";
 import { Hangman } from "./hangman.js";
 import { Word } from "./word.js";
 
 // - - - - - - - - - - || CALLING CLASSES || - - - - - - - - - - //
+
 const keyboard = new Keyboard();
 const hangman = new Hangman();
 const word = new Word();
 
-var wrongGuesses = 0;
-var maxGuesses = 6;
+// - - - - - - - - - - || CALLING HTML || - - - - - - - - - - //
+
+const keyboardContainer = document.getElementById('keyboard_container');
+const wordContainer = document.getElementById('word_container');
+const hintContainer = document.getElementById('hint_container');
+const imgContainer = document.getElementById('img_container');
+const tryContainer = document.getElementById('try_container');
+const gameModal = document.getElementById("game_modal");
+const playAgain = document.getElementById("play_again");
+
+// - - - - - - - - - - || VARIABLES || - - - - - - - - - - //
+
+var wrongGuesses;
+const maxGuesses = 6;
 var currentWord;
+var correctLetters;
 
 // - - - - - - - - - - || FUNCTIONS || - - - - - - - - - - //
+
+const resetGame = () => {
+
+    correctLetters =[];
+    wrongGuesses = 0;
+
+    imgContainer.src = hangman.images[wrongGuesses];
+    tryContainer.textContent = `${wrongGuesses} / ${maxGuesses}`;
+
+    keyboardContainer.querySelectorAll("button").forEach(btn => btn.disabled = false);
+
+    //Display word . . .
+    const wordContainer = document.getElementById('word_container');
+    wordContainer.innerHTML = currentWord.split("").map(() => `<div class="word_letter">_</div>`).join("");
+    gameModal.classList.remove("show");
+}
+
 function displayWord()
 {
-    const wordContainer = document.getElementById('word_container');
-    const hintContainer = document.getElementById('hint_container');
-
     //Get a random word and hint . . . 
     const randomWord = word.wordList[Math.floor(Math.random() * word.wordList.length)];
 
     currentWord = randomWord.word;
     console.log(randomWord.word);
 
-    //Display word . . .
-    wordContainer.innerHTML = randomWord.word.split("").map(() => `<div class="word_letter">_</div>`).join("");
-
     //Display hint . . .
     const span = document.createElement("span");
     span.textContent = "(" + randomWord.hint + ")";
     hintContainer.appendChild(span);
+
+    resetGame();
 }
 
+const gameOver = (isVictory) => {
+
+    setTimeout(() => {
+
+        const modalText = isVictory ? "You found the word:" : "The correct word was:";
+        gameModal.querySelector("h4").innerText = `${isVictory ? "Congrats!" : "Game Over"}`;
+        gameModal.querySelector("p").innerText = `${modalText} <b>${currentWord}</b>`;
+        gameModal.classList.add("show");
+
+    }, 300);
+
+}
 
 // function displayKeyboard()
 // {
@@ -100,16 +140,13 @@ function displayWord()
 
 const initGame = (button, clickedLetter) => {
 
-    const wordContainer = document.getElementById('word_container');
-    const imgContainer = document.getElementById('img_container');
-    const tryContainer = document.getElementById('try_container');
-
     if(currentWord.includes(clickedLetter))
     {
         [...currentWord].forEach((letter,index) => {
 
             if(letter === clickedLetter)
             {
+                correctLetters.push(letter);
                 wordContainer.querySelectorAll("div")[index].innerText = letter;
             }
         });
@@ -122,12 +159,13 @@ const initGame = (button, clickedLetter) => {
 
     button.disabled = true;
     tryContainer.textContent = `${wrongGuesses} / ${maxGuesses}`;
+
+    if(wrongGuesses === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
 }
 
 function displayKeyboard()
 {
-    const keyboardContainer = document.getElementById('keyboard_container');
-
     for(let counter = 97; counter <= 122; counter++)
     {
         const button = document.createElement("button");
@@ -139,6 +177,5 @@ function displayKeyboard()
 }
 
 displayWord();
-
-//displayHangman();
 displayKeyboard();
+playAgain.addEventListener("click", displayWord);
